172fe81c24cd86530d41468afa0bbda408994f6c
saroj <kandelsoftware@gmail.com>
2016-09-14 01:28:06 +0200

    change in file

--- app/models/post.rb
+++
@@ -1,3 +0,0 @@
-class Post < ActiveRecord::Base
-  attr_accessible :comment, :name
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 SampleApp::Application.routes.draw do
-  resources :posts
-
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
